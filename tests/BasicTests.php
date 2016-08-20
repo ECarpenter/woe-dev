195514355a19a858e82198fd3c9b3403d891c470
= <=>
2016-08-20 12:42:00 -0700

    first test file

---
+++ tests/BasicTests.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class BasicTests extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    
+    use WithoutMiddleware;
+    
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
