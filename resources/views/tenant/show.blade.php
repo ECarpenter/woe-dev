ff85aae99c84f3c97bc144f27b5ef8132c8f0293
= <=>
2016-09-14 00:52:44 -0700

    change security to hide insurance

--- resources/views/tenant/show.blade.php
+++ resources/views/tenant/show.blade.php
@@ -49,51 +49,53 @@
 			{{$tenant->unit}} 
 			</small></h4>
 		</div>
-		<div class="col-xs-4">
-			<ul class="nav nav-pills nav-stacked">
-				<li class="dropdown">
-				    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-				    Insurance Requirements<span class="caret"></span>
-				    </a>
+		@permission('manage-insurance'))
+			<div class="col-xs-4">
+				<ul class="nav nav-pills nav-stacked">
+					<li class="dropdown">
+					    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+					    Insurance Requirements<span class="caret"></span>
+					    </a>
 
-				    <ul class="dropdown-menu" role="menu">
-						<li><a href="#">Liability Single - {{ $tenant->req_liability_single_limit == 0 ? number_format($tenant->property->req_liability_single_limit).' (property)' : number_format($tenant->req_liability_single_limit) }}</a></li>
-						<li><a href="#">Liability Combined - {{ $tenant->req_liability_combined_limit == 0 ? number_format($tenant->property->req_liability_combined_limit).' (property)' : number_format($tenant->req_liability_combined_limit) }}</a></li>
-						<li><a href="#">Excess/Umbrella - {{ $tenant->req_umbrella_limit == 0 ? number_format($tenant->property->req_umbrella_limit).' (property)' : number_format($tenant->req_umbrella_limit) }}</a></li>
-						<li><a href="#">Auto - {{ $tenant->req_auto_limit == 0 ? number_format($tenant->property->req_auto_limit).' (property)' : number_format($tenant->req_auto_limit) }}</a></li>
-						<li><a href="#">Workers Comp - {{ $tenant->req_workerscomp_limit == 0 ? number_format($tenant->property->req_workerscomp_limit).' (property)' : number_format($tenant->req_workerscomp_limit) }}</a></li>
-						<li><a href="#" class="open-tenant-req-insurance-modal">Edit Requirements</a></li>
-					</ul>
-				</li>
-			</ul>		
-
-			
-		</div>
+					    <ul class="dropdown-menu" role="menu">
+							<li><a href="#">Liability Single - {{ $tenant->req_liability_single_limit == 0 ? number_format($tenant->property->req_liability_single_limit).' (property)' : number_format($tenant->req_liability_single_limit) }}</a></li>
+							<li><a href="#">Liability Combined - {{ $tenant->req_liability_combined_limit == 0 ? number_format($tenant->property->req_liability_combined_limit).' (property)' : number_format($tenant->req_liability_combined_limit) }}</a></li>
+							<li><a href="#">Excess/Umbrella - {{ $tenant->req_umbrella_limit == 0 ? number_format($tenant->property->req_umbrella_limit).' (property)' : number_format($tenant->req_umbrella_limit) }}</a></li>
+							<li><a href="#">Auto - {{ $tenant->req_auto_limit == 0 ? number_format($tenant->property->req_auto_limit).' (property)' : number_format($tenant->req_auto_limit) }}</a></li>
+							<li><a href="#">Workers Comp - {{ $tenant->req_workerscomp_limit == 0 ? number_format($tenant->property->req_workerscomp_limit).' (property)' : number_format($tenant->req_workerscomp_limit) }}</a></li>
+							<li><a href="#" class="open-tenant-req-insurance-modal">Edit Requirements</a></li>
+						</ul>
+					</li>
+				</ul>		
+			</div>
+		@endpermission
 	</div>
 
 	<div class="row">
 		<div class="col-xs-2 col-xs-offset-2">
-		<ul class="nav nav-pills nav-stacked">
-			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-				  Users<span class="caret"></span>
-				</a>
+			<ul class="nav nav-pills nav-stacked">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+					  Users<span class="caret"></span>
+					</a>
 
-				<ul class="dropdown-menu" role="menu">
-					@foreach ($tenant->user as $user)
-						<li><a href="/user/{{$user->id}}">{{$user->name}}</a></li>
-					@endforeach
-				</ul>
-			</li>
-		</ul>
+					<ul class="dropdown-menu" role="menu">
+						@foreach ($tenant->user as $user)
+							<li><a href="/user/{{$user->id}}">{{$user->name}}</a></li>
+						@endforeach
+					</ul>
+				</li>
+			</ul>
 		</div>
-		<div class="col-xs-4 col-xs-offset-2">
-			<h4> Ins. Notice Sent - <small>
-			@if ($tenant->insurance->last_notice_sent != null)		
-			{{date('F d, Y, g:i a', strtotime($tenant->insurance->last_notice_sent->timezone(Auth::user()->timezone)))}}
-			@endif
-			</small></h4>
-		</div>
+		@permission('manage-insurance'))
+			<div class="col-xs-4 col-xs-offset-2">
+				<h4> Ins. Notice Sent - <small>
+					@if ($tenant->insurance->last_notice_sent != null)		
+						{{date('F d, Y, g:i a', strtotime($tenant->insurance->last_notice_sent->timezone(Auth::user()->timezone)))}}
+					@endif
+				</small></h4>
+			</div>
+		@endpermission
 	</div>
 
 	<div class="row">
@@ -107,102 +109,107 @@
 	</div>
 
 	<br>
+	@permission('manage-insurance'))
+		<div class="row">
+			<div class="col-xs-3 col-xs-offset-3 col-md-3 col-md-offset-3">
+				<button class="btn btn-primary open-manage-insurance-modal btn-xs" value="{{$tenant->insurance->id}}"}}">	Manage Insurance Information</button>
+			</div>
+			<div class="col-xs-3  col-md-3 ">
+				@if ($state['manual_notice'] == "valid")
+				<button class="btn btn-primary btn-xs" type="button" id="btn-send-notice">	Send Insurance Notice </button>
+				<input type="hidden" id="tenant_id" name="tenant_id" value={{$tenant->id}}>
+				@else
+				<a href="#" class="btn btn-primary btn-xs disabled" role="button">	Send Insurance Notice </a>
+				@endif
+			</div>
+		</div>
+		<div id="InformationDisplay">
+			<div id="InsuranceDisplay">
+				<div class="row">
+					<div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-3 text-center">
+						<h4> Insurance </h4>
+					</div>
+					
+				</div>
 
-	<div class="row">
-		<div class="col-xs-3 col-xs-offset-3 col-md-3 col-md-offset-3">
-			<button class="btn btn-primary open-manage-insurance-modal btn-xs" value="{{$tenant->insurance->id}}"}}">	Manage Insurance Information</button>
-		</div>
-		<div class="col-xs-3  col-md-3 ">
-			@if ($state['manual_notice'] == "valid")
-			<button class="btn btn-primary btn-xs" type="button" id="btn-send-notice">	Send Insurance Notice </button>
-			<input type="hidden" id="tenant_id" name="tenant_id" value={{$tenant->id}}>
-			@else
-			<a href="#" class="btn btn-primary btn-xs disabled" role="button">	Send Insurance Notice </a>
-			@endif
-		</div>
-	</div>
-	<div id="InformationDisplay">
-		<div id="InsuranceDisplay">
-			<div class="row">
-				<div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-3 text-center">
-					<h4> Insurance </h4>
-				</div>
-				
-			</div>
-
-			<div class="row">
-				<div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-3">	
-					<table class="table table-hover">
-							<tr class="info">
-								<th>Insurance Type</th>
-								<th>Start Date</th>
-								<th>End Date</th>
-								<th>Limit</th>
-							</tr>
-							
-							<tr onclick="{{$state['llink']}}">
-								<td class="{{$state['lfile']}}">Liability</td>
-								<td class="{{$state['lexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->liability_start))}}</td>
-								<td class="{{$state['lexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->liability_end))}}</td>
-								<td class="{{$state['llimit']}}">{{number_format($tenant->Insurance->liability_single_limit)}} / {{number_format($tenant->Insurance->liability_combined_limit)}}</td>
-							</tr>
-							<tr onclick="{{$state['ulink']}}">
-								<td class="{{$state['ufile']}}">Excess/Umbrella</td>
-								<td class="{{$state['uexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->umbrella_start))}}</td>
-								<td class="{{$state['uexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->umbrella_end))}}</td>
-								<td class="{{$state['ulimit']}}">{{number_format($tenant->Insurance->umbrella_limit)}}</td>
-							</tr>
-							<tr onclick="{{$state['alink']}}">
-								<td class="{{$state['afile']}}">Auto</td>
-								<td class="{{$state['aexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->auto_start))}}</td>
-								<td class="{{$state['aexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->auto_end))}}</td>
-								<td class="{{$state['alimit']}}">{{number_format($tenant->Insurance->auto_limit)}}</td>
-							</tr>
-							@if ($tenant->insurance->workerscomp_applicable)
-							<tr onclick="{{$state['wlink']}}">
-								<td class="{{$state['wfile']}}">Workers Comp</td>
-								<td class="{{$state['wexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->workerscomp_start))}}</td>
-								<td class="{{$state['wexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->workerscomp_end))}}</td>
-								<td class="{{$state['wlimit']}}">{{number_format($tenant->Insurance->workerscomp_limit)}}</td>
-							</tr>
-							@endif
-							<tr onclick="{{$state['elink']}}">
-								<td class="{{$state['efile']}} text-center" colspan="4" >Endorsement</td>
-							</tr>
-					</table>
+				<div class="row">
+					<div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-3">	
+						<table class="table table-hover">
+								<tr class="info">
+									<th>Insurance Type</th>
+									<th>Start Date</th>
+									<th>End Date</th>
+									<th>Limit</th>
+								</tr>
+								
+								<tr onclick="{{$state['llink']}}">
+									<td class="{{$state['lfile']}}">Liability</td>
+									<td class="{{$state['lexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->liability_start))}}</td>
+									<td class="{{$state['lexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->liability_end))}}</td>
+									<td class="{{$state['llimit']}}">{{number_format($tenant->Insurance->liability_single_limit)}} / {{number_format($tenant->Insurance->liability_combined_limit)}}</td>
+								</tr>
+								<tr onclick="{{$state['ulink']}}">
+									<td class="{{$state['ufile']}}">Excess/Umbrella</td>
+									<td class="{{$state['uexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->umbrella_start))}}</td>
+									<td class="{{$state['uexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->umbrella_end))}}</td>
+									<td class="{{$state['ulimit']}}">{{number_format($tenant->Insurance->umbrella_limit)}}</td>
+								</tr>
+								<tr onclick="{{$state['alink']}}">
+									<td class="{{$state['afile']}}">Auto</td>
+									<td class="{{$state['aexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->auto_start))}}</td>
+									<td class="{{$state['aexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->auto_end))}}</td>
+									<td class="{{$state['alimit']}}">{{number_format($tenant->Insurance->auto_limit)}}</td>
+								</tr>
+								@if ($tenant->insurance->workerscomp_applicable)
+								<tr onclick="{{$state['wlink']}}">
+									<td class="{{$state['wfile']}}">Workers Comp</td>
+									<td class="{{$state['wexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->workerscomp_start))}}</td>
+									<td class="{{$state['wexpire']}}">{{date('F d, Y', strtotime($tenant->Insurance->workerscomp_end))}}</td>
+									<td class="{{$state['wlimit']}}">{{number_format($tenant->Insurance->workerscomp_limit)}}</td>
+								</tr>
+								@endif
+								<tr onclick="{{$state['elink']}}">
+									<td class="{{$state['efile']}} text-center" colspan="4" >Endorsement</td>
+								</tr>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
-		@permission('manage-wo')
-		<div id="WorkorderDisplay">
-			<div class="row">
-				<div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-3 text-center">
-					<h4> Work Order </h4>
-				</div>
-				
-			</div>
-
-			<div class="row">
-				<div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-3">	
-					<table class="table table-hover">
-							<tr class="info">
-								<th>Type</th>
-								<th>Status</th>
-								<th>Date</th>
-							</tr>
-							@foreach ($tenant->Workorder()->orderBy('created_at','desc')->get() as $workorder)
-							<tr onclick = "location.href='/workorders/{{$workorder->id}}'">
-								<td>{{$workorder->ProblemType->type}}</td>
-								<td>{{$workorder->status}}</td>
-								<td>{{date('F d, Y, g:i a', strtotime($workorder->created_at->timezone(Auth::user()->timezone)))}}</td>
-							</tr>
-							@endforeach
-					</table>
-				</div>
-			</div>
-		</div>
-	</div>
 	@endpermission
+	@permission('manage-wo')
+		<div id="InformationDisplay">
+			<div id="WorkorderDisplay">
+				<div class="row">
+					<div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-3 text-center">
+						<h4> Work Order </h4>
+					</div>
+					
+				</div>
+
+				<div class="row">
+					<div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-3">	
+						<table class="table table-hover">
+								<tr class="info">
+									<th>Type</th>
+									<th>Status</th>
+									<th>Date</th>
+								</tr>
+								@foreach ($tenant->Workorder()->orderBy('created_at','desc')->get() as $workorder)
+								<tr onclick = "location.href='/workorders/{{$workorder->id}}'">
+									<td>{{$workorder->ProblemType->type}}</td>
+									<td>{{$workorder->status}}</td>
+									<td>{{date('F d, Y, g:i a', strtotime($workorder->created_at->timezone(Auth::user()->timezone)))}}</td>
+								</tr>
+								@endforeach
+						</table>
+					</div>
+				</div>
+			</div>	
+		</div>
+	@endpermission
+		
+
 
 	<!-- ManageInsuranceModal (Pop up when manage button clicked) -->
 	<div class="modal fade" id="ManageInsuranceModal" tabindex="-1" role="dialog" aria-labelledby="ManageModalLabel" aria-hidden="true">
