7e7c8d7ff00a463d5cecf4a365944303980cb565
= <=>
2017-12-02 19:41:56 -0800

    Add standard session flashes to the main page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -174,6 +174,34 @@
         </div>
     </nav>
 
+    @if(Session::has('success'))
+        <div class="alert alert-success alert-dismissible text-center col-xs-10 col-xs-offset-1 col-md-7 col-md-offset-2" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            {{ Session::get('success') }}
+        </div>  
+    @endif
+
+    @if(Session::has('info'))
+        <div class="alert alert-info alert-dismissible text-center col-xs-10 col-xs-offset-1 col-md-7 col-md-offset-2" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            {{ Session::get('info') }}
+        </div>  
+    @endif
+
+    @if(Session::has('warning'))
+        <div class="alert alert-warning alert-dismissible text-center col-xs-10 col-xs-offset-1 col-md-7 col-md-offset-2" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            {{ Session::get('warning') }}
+        </div>  
+    @endif
+
+    @if(Session::has('danger'))
+        <div class="alert alert-danger alert-dismissible text-center col-xs-10 col-xs-offset-1 col-md-7 col-md-offset-2" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            {{ Session::get('danger') }}
+        </div>  
+    @endif
+
     @yield('content')
 
     <!-- JavaScripts -->
