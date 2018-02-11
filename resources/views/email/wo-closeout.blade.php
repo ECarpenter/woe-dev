4cb075d894120df0c83d91ad3b9b98fdc760c8e0
= <=>
2018-02-11 09:20:54 -0800

    Edit close out message

--- resources/views/email/wo-closeout.blade.php
+++ resources/views/email/wo-closeout.blade.php
@@ -1,6 +1,9 @@
-Thank you for using our Work order system. The recent <a href="{{env('APP_URL')}}/workorders/{{$workorder->id}}">work order,</a> for {{$workorder->Property()->name}} that you sent has been completed. If you have any further issues please submit a followup message <a href="{{env('APP_URL')}}/workorders/{{$workorder->id}}">here.</a> 
-
+Thank you for using our Work order system. The recent <a href="{{env('APP_URL')}}/workorders/{{$workorder->id}}">work order,</a> for {{$workorder->Property()->name}} that you sent has been completed. 
+<br><br>
+If you have any further issues please submit a followup message <a href="{{env('APP_URL')}}/workorders/{{$workorder->id}}">here.</a> 
+<br><br>
 Thank you,
-
+<br>
 {{Auth::user()->name}}
+<br><br>
 
